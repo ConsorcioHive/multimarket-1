e69fe5dd34ea176b26991f3d3a4206fa570444f7
cperaza63 <cpdigitalsolution@yahoo.com.ve>
2024-04-28 13:42:00 -0400

    despuesdearreglarplugins

--- app/views/inc/script.php
+++ app/views/inc/script.php
@@ -1,2 +1,19 @@
-<script src="<?php echo APP_URL; ?>app/views/js/ajax.js" ></script>
-<script src="<?php echo APP_URL; ?>app/views/js/main.js" ></script>
+ <!-- JAVASCRIPT -->
+    <script src="http://localhost/multimarket/library/html/material/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="http://localhost/multimarket/library/html/material/assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="http://localhost/multimarket/library/html/material/assets/libs/node-waves/waves.min.js"></script>
+    <script src="http://localhost/multimarket/library/html/material/assets/libs/feather-icons/feather.min.js"></script>
+    <script src="http://localhost/multimarket/library/html/material/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="http://localhost/multimarket/library/html/material/assets/js/plugins.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/choices.js/1.1.6/choices.min.js"></script>
+    <!--Swiper slider js-->
+    <script src="http://localhost/multimarket/library/html/material/assets/libs/swiper/swiper-bundle.min.js"></script>
+    <!-- landing init -->
+    <script src="http://localhost/multimarket/library/html/material/assets/js/pages/landing.init.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.6.13/flatpickr.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/choices.js/10.2.0/choices.min.js"></script>
+
+    <!-- javascript hechos por mi -->
+    <script src="<?php echo APP_URL; ?>app/views/js/ajax.js" ></script>
+    <script src="<?php echo APP_URL; ?>app/views/js/main.js" ></script>
